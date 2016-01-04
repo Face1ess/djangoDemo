7608b8456ac91c10dd08b2305fbddfac8c8dcbe6
faceless garfield <thefaceless819@sina.com>
2016-01-04 11:48:33 +0800

    decorate my template

--- mysite/run/views.py
+++ mysite/run/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response
 from run.models import T_Act
 from django.core.paginator import Paginator
+from django.core.paginator import PageNotAnInteger
+from django.core.paginator import EmptyPage
 
 # Create your views here.
 def runInfo(request):
