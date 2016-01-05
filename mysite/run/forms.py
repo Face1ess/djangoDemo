a2da8c05863926fccb84a326eb0249bbdbe9f54e
PengGarfield <PG@PengGarfielddeMacBook-Pro.local>
2016-01-05 22:59:18 +0800

    form is done!

--- mysite/run/forms.py
+++ mysite/run/forms.py
@@ -1,14 +1,11 @@
+#coding=utf-8
 from django import forms
 
-class ContactForm(forms.Form):
-    subject = forms.CharField()
-    email = forms.EmailField(required=False)
-    message = forms.CharField()
 
 class ActForm(forms.Form):
-    actDist = forms.DecimalField()
-    actType = forms.CharField()
-    actDuration = forms.CharField()
-    actStartDate = forms.CharField()
-    actAvgSpd = forms.DecimalField()
-    actCalBurn = forms.IntegerField()
+    actDist = forms.DecimalField(label='运动距离')
+    actType = forms.CharField(label='运动类型')
+    actDuration = forms.CharField(label='运动持续时间')
+    actStartDate = forms.CharField(label='开始日期')
+    actAvgSpd = forms.DecimalField(label='平均速度')
+    actCalBurn = forms.IntegerField(label='消耗卡路里')
--- mysite/run/views.py
+++ mysite/run/views.py
@@ -8,45 +8,23 @@ from run.forms import ContactForm,ActForm
 
 # Create your views here.
 def runInfo(request):
-    itemData = T_Act.objects.all().order_by('-id')
-    paginator = Paginator(itemData,8)
-    page = request.GET.get('page')
-    try:
-        itemList = paginator.page(page)
-    except PageNotAnInteger :
-        itemList = paginator.page(1)
-    except EmptyPage:
-        itemList = paginator.page(paginator.num_pages)
-    return render_to_response('runMainTemplate.html',{"itemList":itemList})
-
-def search_form(request):
-    return render_to_response('search_form.html')
-
-def search(request):
-    if 'q' in request.GET:
-        message = 'You searched for: %r' % request.GET['q']
-    else:
-        message = 'You submitted an empty form.'
-    return HttpResponse(message)
-
-def contact_form(request):
-    if request.method == 'POST' :
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            return HttpResponse(cd['message'])
-    else:
-        form = ContactForm()
-    return render_to_response('contact_form.html',{'form':form})
-
-def act_form(request):
     if request.method == 'POST' :
         form = ActForm(request.POST)
         if form.is_valid():
-            return HttpResponse("Success")
-        else :
-            return HttpResponse("Failure")
-    else :
+             cd = form.cleaned_data
+             act = T_Act(T_ActDist=cd['actDist'], T_ActType=cd['actType'], T_ActDuration=cd['actDuration'], T_ActStartDate=cd['actStartDate'], T_ActAvgSpd=cd['actAvgSpd'], T_ActCalBurn=cd['actCalBurn'])
+             act.save()
+             return HttpResponseRedirect('/run/')
+    else:
+        itemData = T_Act.objects.all().order_by('-id')
+        paginator = Paginator(itemData,8)
+        page = request.GET.get('page')
         form = ActForm()
-    return render_to_response('act_form.html',{'form':form})
+        try:
+            itemList = paginator.page(page)
+        except PageNotAnInteger :
+            itemList = paginator.page(1)
+        except EmptyPage:
+            itemList = paginator.page(paginator.num_pages)
+        return render_to_response('runMainTemplate.html',{"itemList":itemList,'form':form})
          
